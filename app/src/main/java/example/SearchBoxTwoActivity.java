abb208394a312aa404b886a6bbfe44a8a1e74f12
snpmyn <snpmyn@126.com>
2019-06-21 16:50:05 +0800

    搜索框优化

--- app/src/main/java/example/SearchBoxTwoActivity.java
+++ app/src/main/java/example/SearchBoxTwoActivity.java
@@ -1,29 +1,25 @@
 package example;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.zsp.library.searchbox.two.fragment.SearchDialogFragment;
-import com.zsp.library.textview.DrawableCenterTextView;
-import com.zsp.utilone.ViewUtils;
-import com.zsp.utilone.animation.AnimationManager;
 import com.zsp.utilone.toast.ToastUtils;
 import com.zsp.widget.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  * @decs: 搜索框二页
  * @author: 郑少鹏
  * @date: 2019/4/29 18:57
  */
-public class SearchBoxTwoActivity extends AppCompatActivity implements SearchDialogFragment.OnSearchClickListener {
-    @BindView(R.id.searchBoxTwoActivityDctvSearch)
-    DrawableCenterTextView searchBoxTwoActivityDctvSearch;
+public class SearchBoxTwoActivity extends AppCompatActivity {
+    @BindView(R.id.searchBoxTwoActivityMt)
+    MaterialToolbar searchBoxTwoActivityMt;
     /**
      * 搜索对话框碎片
      */
@@ -34,37 +30,23 @@ public class SearchBoxTwoActivity extends AppCompatActivity implements SearchDia
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_box_two);
         ButterKnife.bind(this);
-        step();
+        stepMaterialToolbar();
+        stepSearchDialogFragment();
     }
 
-    private void step() {
-        searchDialogFragment = SearchDialogFragment.newInstance();
-        searchDialogFragment.setOnSearchClickListener(this);
-        searchDialogFragment.setOnSearchDialogHideListener(() -> {
-            ViewUtils.showView(searchBoxTwoActivityDctvSearch);
-            AnimationManager.xGradual(searchBoxTwoActivityDctvSearch, 0, searchBoxTwoActivityDctvSearch.getWidth(), 300, 1);
+    private void stepMaterialToolbar() {
+        searchBoxTwoActivityMt.inflateMenu(R.menu.search_menu);
+        searchBoxTwoActivityMt.setNavigationOnClickListener(v -> finish());
+        searchBoxTwoActivityMt.setOnMenuItemClickListener(item -> {
+            searchBoxTwoActivityMt.getMenu().findItem(R.id.pharmaceuticalKnowledgeListActivityMenuSearch).setVisible(false);
+            searchDialogFragment.showFragment(getSupportFragmentManager(), "SearchDialogFragment");
+            return true;
         });
     }
 
-    @OnClick({R.id.searchBoxTwoActivityIvTopBack, R.id.searchBoxTwoActivityDctvSearch})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            // 返
-            case R.id.searchBoxTwoActivityIvTopBack:
-                finish();
-                break;
-            // 搜索框
-            case R.id.searchBoxTwoActivityDctvSearch:
-                ViewUtils.hideView(searchBoxTwoActivityDctvSearch, View.GONE);
-                searchDialogFragment.showFragment(getSupportFragmentManager(), SearchDialogFragment.TAG);
-                break;
-            default:
-                break;
-        }
-    }
-
-    @Override
-    public void onSearchClick(String keyword) {
-        ToastUtils.shortShow(this, keyword);
+    private void stepSearchDialogFragment() {
+        searchDialogFragment = SearchDialogFragment.newInstance();
+        searchDialogFragment.setOnSearchClickListener(keyword -> ToastUtils.shortShow(this, keyword));
+        searchDialogFragment.setOnSearchDialogHideListener(() -> searchBoxTwoActivityMt.getMenu().findItem(R.id.pharmaceuticalKnowledgeListActivityMenuSearch).setVisible(true));
     }
 }
