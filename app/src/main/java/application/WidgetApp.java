2767205f90471ec3ad7c2087c26dc13553d1cb3a
snpmyn <snpmyn@126.com>
2019-10-19 17:14:30 +0800

    优化

--- app/src/main/java/application/WidgetApp.java
+++ app/src/main/java/application/WidgetApp.java
@@ -1,11 +1,15 @@
 package application;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.multidex.MultiDex;
 
 import com.zsp.library.status.manager.StatusManager;
+import com.zsp.utilone.activity.ActivitySuperviseManager;
 import com.zsp.utilone.timber.configure.TimberInitConfigure;
 import com.zsp.widget.BuildConfig;
 import com.zsp.widget.R;
@@ -31,7 +35,54 @@ public class WidgetApp extends Application {
      * 初始化配置
      */
     private void initConfiguration() {
+        // timber
         TimberInitConfigure.initTimber(BuildConfig.DEBUG);
+        // 全局监听Activity生命周期
+        registerActivityListener();
+    }
+
+    /**
+     * Activity全局监听
+     */
+    private void registerActivityListener() {
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(@NonNull Activity activity, Bundle savedInstanceState) {
+                // 添监听到创事件Activity至集合
+                ActivitySuperviseManager.pushActivity(activity);
+            }
+
+            @Override
+            public void onActivityStarted(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(@NonNull Activity activity) {
+                // 移监听到销事件Activity出集合
+                ActivitySuperviseManager.removeActivity(activity);
+            }
+        });
     }
 
     @Override
